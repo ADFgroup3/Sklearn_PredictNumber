b13b99a1c8d633d4ab9b1e62d1f8ac711c753414
Yuto Hayama <55091908+hayamayuto@users.noreply.github.com>
2021-12-08 03:19:09 +0900

    Update canvas.js

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", () => {
+    //DOMツリーが構築され次第実行（サイトが表示される直前に実行）
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
 
@@ -12,11 +13,11 @@ window.addEventListener("load", () => {
 
     let isDrag = false;
 
-        //書く処理（主要部）
-        function draw(x, y){
-            if(!isDrag){
-                return;
-            }
+    //書く処理(メイン)
+    function draw(x, y){
+        if(!isDrag){
+            return;
+        }
         //ライン指定
         ctx.lineCap = "round";
         ctx.lineJoin = "round";
@@ -61,4 +62,26 @@ window.addEventListener("load", () => {
     canvas.addEventListener("mousemove", (e) => {
         draw(e.layerX, e.layerY);
     });
+
+    const canvasP = canvas.getBoundingClientRect();
+    canvas.addEventListener("touchstart", dragStart);
+    canvas.addEventListener("touchend", dragEnd);
+    canvas.addEventListener("touchcancel", dragEnd);
+    canvas.addEventListener("touchmove", (e) => {
+        let deltaX = e.touches[0].pageX - canvasP.left;
+        let deltaY = e.touches[0].pageY - canvasP.top;
+        draw(deltaX, deltaY);
+    });
 });
+
+//submitボタンが押されたときの処理
+function CanvasToImage(){
+    const canvas = document.getElementById("canvas");
+    const dataURL = canvas.toDataURL('image/png').replace(/^.*,/, '');
+
+    //隠し要素に値を代入
+    document.getElementById("img").value = dataURL;
+    
+    //B64型式の画像を表示
+    console.log(dataURL);
+}
